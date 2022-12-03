abe687e49c2397da416bfa85919587c7a9b003af
vanyapanf <50085942+vanyapanf@users.noreply.github.com>
2022-12-03 17:14:05 +0300

    initial commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Checkbox, Button, ListItem, Typography } from "@material-ui/core";
-import { Card, CardContent, CardActions, Chip, Stack } from '@mui/material';
+import { Card, CardContent, CardActions, Chip, Stack } from "@mui/material";
 
 function Todo({ todo, toggleComplete, removeTodo, handleOpenDialog }) {
-
   function handleCheckboxClick() {
     toggleComplete(todo.id);
   }
@@ -17,45 +16,32 @@ function Todo({ todo, toggleComplete, removeTodo, handleOpenDialog }) {
   }
 
   return (
-    <Card
-      sx={{ marginTop: 1}}
-    >
+    <Card sx={{ mt: 1, width: "100%", maxWidth: 500 }}>
       <CardContent>
         <Stack direction="row" spacing={1}>
-          <Chip label={todo.date? todo.date : 'no Date'} />
-          <Chip label={todo.endDate? todo.endDate : 'no endDate'} />
-          <Chip label={todo.author? todo.author : 'no Author'} />
+          <Chip label={todo.date ? todo.date : "no Date"} />
+          <Chip label={todo.endDate ? todo.endDate : "no endDate"} />
+          <Chip label={todo.author ? todo.author : "no Author"} />
         </Stack>
-        <Typography
-          sx={{ marginTop: 10}}
-        >
-          {todo.task}
-        </Typography>
+        <Typography sx={{ marginTop: 10 }}>{todo.task}</Typography>
       </CardContent>
       <CardActions>
         <Stack direction="row" spacing={1}>
-          {
-            !todo.completed ?
-            <Button onClick={handleCheckboxClick}>
-              Отметить задачу
-            </Button>
-            :
-            <Button 
-              variant="contained" 
-              onClick={handleCheckboxClick} 
+          {!todo.completed ? (
+            <Button onClick={handleCheckboxClick}>Отметить задачу</Button>
+          ) : (
+            <Button
+              variant="contained"
+              onClick={handleCheckboxClick}
               style={{
-                backgroundColor: "#1976d2",
+                backgroundColor: "#1976d2"
               }}
             >
               Задача сделана
             </Button>
-          }
-          <Button onClick={handleEditTodo}>
-            Редактировать
-          </Button>
-          <Button onClick={handleRemoveClick}>
-            Закрыть задачу
-          </Button>
+          )}
+          <Button onClick={handleEditTodo}>Редактировать</Button>
+          <Button onClick={handleRemoveClick}>Закрыть задачу</Button>
         </Stack>
       </CardActions>
     </Card>
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import { List, TextField } from "@material-ui/core";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+} from "@mui/material";
 import CloseIcon from "@material-ui/icons/Close";
-import EditTodoForm from "../Components/EditTodoForm"
+import EditTodoForm from "../Components/EditTodoForm";
 import Todo from "./Todo";
 
 function TodoList({ todos, setTodos }) {
-
-  const [todoDate, setTodoDate] = useState(new Date().toISOString().slice(0, 10)); 
+  const [todoDate, setTodoDate] = useState(
+    new Date().toISOString().slice(0, 10)
+  );
 
   const [openDialog, setOpenDialog] = React.useState(false);
 
   const [editTodo, setEditTodo] = React.useState();
- 
+
   const handleOpenDialog = (todo) => {
     setEditTodo(todo);
     setOpenDialog(true);
@@ -42,51 +50,46 @@ function TodoList({ todos, setTodos }) {
 
   return (
     <div>
-    <Dialog open={openDialog} onClose={handleCloseDialog}>
-      <DialogTitle>Редактирование задачи</DialogTitle>
-      <DialogContent>
-        <EditTodoForm editTodo={editTodo} todos={todos} setTodos={setTodos} />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleCloseDialog}>Отмена</Button>
-        <Button
-          form="form"
-          type="submit"
-          variant="contained"
-          onClick={handleCloseDialog}
-        >
-          Сохранить
-        </Button>
-      </DialogActions>
-    </Dialog>
-    <List 
-      alignItems="center"
-    >
-      <TextField
-        name="startDate"
-        label="Выбрать дату"
-        InputLabelProps={{ shrink: true, required: true }}
-        type="date"
-        value={todoDate}
-        onChange={(e) => setTodoDate(e.target.value)}
-      />
-
-      {
-        todos
-        .filter((todo) => (
-          todo.date == todoDate
-        ))
-        .map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            toggleComplete={toggleComplete}
-            handleOpenDialog={handleOpenDialog}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Редактирование задачи</DialogTitle>
+        <DialogContent>
+          <EditTodoForm editTodo={editTodo} todos={todos} setTodos={setTodos} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Отмена</Button>
+          <Button
+            form="form"
+            type="submit"
+            variant="contained"
+            onClick={handleCloseDialog}
+          >
+            Сохранить
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <List alignItems="center">
+        <Box sx={{ textAlign: "center" }}>
+          <TextField
+            name="startDate"
+            label="Выбрать дату"
+            InputLabelProps={{ shrink: true, required: true }}
+            type="date"
+            value={todoDate}
+            onChange={(e) => setTodoDate(e.target.value)}
           />
-        ))
-      }
-    </List>
+        </Box>
+        {todos
+          .filter((todo) => todo.date == todoDate)
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              toggleComplete={toggleComplete}
+              handleOpenDialog={handleOpenDialog}
+            />
+          ))}
+      </List>
     </div>
   );
 }
