1efeee0aaea256c37aab1e6121c6e0cbe7c1ceda
Ivan Panfilov <ivan.panf@yandex.ru>
2022-12-03 12:25:57 +0300

    немного покрасивей

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { List, TextField } from "@material-ui/core";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import CloseIcon from "@material-ui/icons/Close";
+import { Button, Box, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import EditTodoForm from "../Components/EditTodoForm"
 import Todo from "./Todo";
 
@@ -41,53 +40,55 @@ function TodoList({ todos, setTodos }) {
   }
 
   return (
-    <div>
-    <Dialog open={openDialog} onClose={handleCloseDialog}>
-      <DialogTitle>Редактирование задачи</DialogTitle>
-      <DialogContent>
-        <EditTodoForm editTodo={editTodo} todos={todos} setTodos={setTodos} />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleCloseDialog}>Отмена</Button>
-        <Button
-          form="form"
-          type="submit"
-          variant="contained"
-          onClick={handleCloseDialog}
-        >
-          Сохранить
-        </Button>
-      </DialogActions>
-    </Dialog>
-    <List 
-      alignItems="center"
-    >
-      <TextField
-        name="startDate"
-        label="Выбрать дату"
-        InputLabelProps={{ shrink: true, required: true }}
-        type="date"
-        value={todoDate}
-        onChange={(e) => setTodoDate(e.target.value)}
-      />
+    <Box sx={{ mt: 2 }}>
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Редактирование задачи</DialogTitle>
+        <DialogContent>
+          <EditTodoForm editTodo={editTodo} todos={todos} setTodos={setTodos} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Отмена</Button>
+          <Button
+            form="form"
+            type="submit"
+            variant="contained"
+            onClick={handleCloseDialog}
+          >
+            Сохранить
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <List 
+        alignItems="center"
+      >
+        <TextField
+          name="startDate"
+          label="Выбрать дату"
+          InputLabelProps={{ shrink: true, required: true }}
+          type="date"
+          value={todoDate}
+          onChange={(e) => setTodoDate(e.target.value)}
+        />
 
-      {
-        todos
-        .filter((todo) => (
-          todo.date == todoDate
-        ))
-        .map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            toggleComplete={toggleComplete}
-            handleOpenDialog={handleOpenDialog}
-          />
-        ))
-      }
-    </List>
-    </div>
+        {
+          todos
+          .filter((todo) => (
+            todo.date == todoDate
+          ))
+          .map((todo) => (
+            <Box sx={{ mt: 2 }}>
+              <Todo
+                key={todo.id}
+                todo={todo}
+                removeTodo={removeTodo}
+                toggleComplete={toggleComplete}
+                handleOpenDialog={handleOpenDialog}
+              />
+            </Box>
+          ))
+        }
+      </List>
+    </Box>
   );
 }
 
